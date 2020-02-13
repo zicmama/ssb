31c6f1d67c94b637c371272082f3c4abeaaaa375
Henry <cryptix@riseup.net>
2020-02-13 23:02:00 +0100

    left over debug print

--- internal/luigiutils/multisink.go
+++ internal/luigiutils/multisink.go
@@ -4,7 +4,6 @@ package luigiutils
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/pkg/errors"
 	"go.cryptoscope.co/luigi"
@@ -50,7 +49,6 @@ func (f *MultiSink) Register(
 	until int64,
 ) error {
 	f.sinks = append(f.sinks, sink)
-	fmt.Printf("DEBUG sink type:%T\n", sink)
 	f.ctxs[sink] = ctx
 	f.until[sink] = until
 	return nil
